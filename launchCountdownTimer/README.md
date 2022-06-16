d98d4208d3db04b7c3ecba264666b7dcffd54888
clarafuwen <clarafuwen@gmail.com>
2022-06-16 18:49:24 -0400

    first commit

---
+++ launchCountdownTimer/README.md
@@ -0,0 +1,53 @@
+# Frontend Mentor - Launch countdown timer solution
+
+This is a solution to the [Launch countdown timer challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/launch-countdown-timer-N0XkGfyz-). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- See hover states for all interactive elements on the page
+- See a live countdown timer that ticks down every second (start the count at 14 days)
+- When a number changes, make the card flip from the middle
+
+### Screenshot
+
+![desktop_screenshot](./screenshot.jpg)
+
+### Links
+
+- Solution URL: [github](https://your-solution-url.com)
+- Live Site URL: [netlify](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+### Useful resources
+
+- [flip card design](https://www.youtube.com/watch?v=p_6IuhmBsfc) - I used the ideas for styling the flip card by "Web Dev Simplified"
+- [countdown timer](https://www.w3schools.com/howto/howto_js_countdown.asp)
+
+## Author
+
+- Website - [Clara Wen](https://www.your-site.com)
+- Frontend Mentor - [@clarafuwen](https://www.frontendmentor.io/profile/clarafuwen)
---
+++ launchCountdownTimer/index.js
@@ -0,0 +1,94 @@
+let figures = document.querySelectorAll(".figures");
+let flipTop = document.querySelectorAll(".cards__item--topFlip");
+let flipBottom = document.querySelectorAll(".cards__item--bottomFlip");
+
+//varibales for each animation
+let ani_d_u, ani_d_b, ani_h_u, ani_h_b, ani_m_u, ani_m_b, ani_s_u, ani_s_b;
+
+const flip = [{ transform: "rotateX(90deg)" }];
+const flipTimeTop = {
+  duration: 250,
+  iterations: 1,
+  easing: "ease-in",
+};
+const flipTimeBottom = {
+  duration: 500,
+  iterations: 1,
+  easing: "ease-in-out",
+};
+
+//set time according to given 8:23:55:41
+let days = 8 * 1000 * 60 * 60 * 24;
+let hours = 23 * 1000 * 60 * 60;
+let minutes = 55 * 1000 * 60;
+let seconds = 42 * 1000;
+
+let days_pre = 8,
+  hours_pre = 23,
+  minutes_pre = 55;
+
+let distance = days + hours + minutes + seconds;
+
+let now = new Date().getTime();
+let future = now + distance;
+let setDate = new Date(future);
+
+let x = setInterval(function () {
+  // Get today's date and time
+  now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  distance = setDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  //flip each figure when changed
+  if (days !== days_pre) {
+    ani_d_u = flipTop[0].animate(flip, flipTimeTop);
+    ani_d_b = flipBottom[0].animate(flip, flipTimeBottom);
+    days_pre = days;
+  }
+
+  if (hours !== hours_pre) {
+    ani_h_u = flipTop[1].animate(flip, flipTimeTop);
+    ani_h_b = flipBottom[1].animate(flip, flipTimeBottom);
+    hours_pre = hours;
+  }
+
+  if (minutes !== minutes_pre) {
+    ani_m_u = flipTop[2].animate(flip, flipTimeTop);
+    ani_m_b = flipBottom[2].animate(flip, flipTimeBottom);
+    minutes_pre = minutes;
+  }
+
+  //always flip card for seconds
+  ani_s_u = flipTop[3].animate(flip, flipTimeTop);
+  ani_s_b = flipBottom[3].animate(flip, flipTimeBottom);
+
+  //add 0 in front when figure less than 10
+  figures[0].textContent = days < 10 ? `0${days}` : `${days}`;
+  figures[1].textContent = days < 10 ? `0${days}` : `${days}`;
+  figures[2].textContent = hours < 10 ? `0${hours}` : `${hours}`;
+  figures[3].textContent = hours < 10 ? `0${hours}` : `${hours}`;
+  figures[4].textContent = minutes < 10 ? `0${minutes}` : `${minutes}`;
+  figures[5].textContent = minutes < 10 ? `0${minutes}` : `${minutes}`;
+  figures[6].textContent = seconds < 10 ? `0${seconds}` : `${seconds}`;
+  figures[7].textContent = seconds < 10 ? `0${seconds}` : `${seconds}`;
+
+  //pause all flip animation except for seconds
+  ani_d_u.pause();
+  ani_d_b.pasue();
+  ani_h_u.pause();
+  ani_h_b.pasue();
+  ani_m_u.pause();
+  ani_m_b.pasue();
+
+  if (distance < 0) {
+    clearInterval(x);
+    figures.innerHTML = "EXPIRED";
+  }
+}, 1000);
